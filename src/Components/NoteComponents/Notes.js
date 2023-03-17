dd5ee3a6425a7ec78d26a2b6079cda4b20474ac3
MikeAriyo <ariyoadekola2@gmail.com>
2023-03-17 11:48:29 +0100

    added function to save a new notes to the states array

--- src/Components/NoteComponents/Notes.js
+++ src/Components/NoteComponents/Notes.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from "react";
 import CreateNote from "./CreateNote";
 import "../css/Note.css";
 import Note from "./Note";
+import { v4 as uuid } from "uuid";
 
 function Notes() {
   const [notes, setNotes] = useState([]);
@@ -9,6 +10,20 @@ function Notes() {
 
   const textHandler = (e) => {
     setInputText(e.target.value);
+  };
+
+  // add new note to the state array
+  const saveHandler = () => {
+    setNotes((prevState) => [
+      ...prevState,
+      {
+        id: uuid(),
+        text: inputText,
+      },
+    ]);
+
+    // clear the text area
+    setInputText("");
   };
   return (
     <div className="notes">
